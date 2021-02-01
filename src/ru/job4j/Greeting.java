505d03a26de48da41fce46aee2f5533ff4d1c42b
John_Smith <the.tylertyler@gmail.com>
2021-02-01 18:49:39 +0300

    9.1. String [#459368]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2021;
+        String space = " ";
+        String ideaPlusYear = idea + year;
+        String ideaPlusSpacePlusYear = idea + space + year;
+
+        System.out.println(idea);
+        System.out.println(idea + " But I am a newbie.");
+        System.out.println(idea + year);
+        System.out.println(ideaPlusYear);
+        System.out.println(ideaPlusSpacePlusYear);
+    }
+}
