97d105516976e5a38c27b050f120e318e3d2486d
NorthMishka <the.tylertyler@gmail.com>
2021-02-09 12:21:55 +0300

    5.4. Протеиновая диета [#459415]

--- test/ru/job4j/FitnessTest.java
+++ test/ru/job4j/FitnessTest.java
@@ -24,4 +24,10 @@ public class FitnessTest {
         assertThat(month, is(2));
     }
 
+    @Test
+    public void whenIvanIsEqualToNik() {
+        int month = Fitness.calc(90, 90);
+        assertThat(month, is(1));
+    }
+
 }
