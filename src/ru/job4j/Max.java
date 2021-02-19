ad5cc8d97082f6e24cc7cf97d480c34be0d051f0
John_Smith <the.tylertyler@gmail.com>
2021-02-19 23:06:59 +0300

    3. Перегрузить метод max для трех чисел [#459517]

--- src/ru/job4j/Max.java
+++ src/ru/job4j/Max.java
@@ -2,12 +2,21 @@ package ru.job4j;
 
 public class Max {
 
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
+//    public static int max(int left, int right) {
+//        return left >= right ? left : right;
+//    }
+    public static  int max(int a, int b){
+        return a + b;
     }
-
-    public static void main(String[] args) {
-        max(12, 11);
-
+    public static  int max(int a, int b, int c){
+        return a + max(b, c);
     }
+    public static int max(int a, int b, int c, int d){
+        return a + max(b, c, d);
+    }
+//
+//    public static void main(String[] args) {
+//        max(12, 11);
+//
+//    }
 }
--- test/ru/job4j/MaxTest.java
+++ test/ru/job4j/MaxTest.java
@@ -8,20 +8,20 @@ import static org.junit.Assert.*;
 public class MaxTest {
 
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax1Plus2Then3() {
         int result = Max.max(1, 2);
-        assertThat(result, is(2));
+        assertThat(result, is(3));
     }
 
     @Test
-    public void whenMax4To2Then4() {
-        int result = Max.max(4, 2);
-        assertThat(result, is(4));
+    public void whenMax4Plus2Plus1Then7() {
+        int result = Max.max(4, 2, 1);
+        assertThat(result, is(7));
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int result = Max.max(2, 2);
-        assertThat(result, is(2));
+    public void whenMax2Plus2Plus2Plus2Then8() {
+        int result = Max.max(2, 2, 2, 2);
+        assertThat(result, is(8));
     }
 }
